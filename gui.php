041284a4a788747d0b03f6eaf0866b1f81d329d8
mohammad <mohamad.agz@gmail.com>
2016-01-23 18:50:44 +0330

    add an entry box and edit sonethings

--- gui.php
+++ gui.php
@@ -11,29 +11,37 @@ class cgui {
 		$win->connect_simple('destroy',array('quit','main_quit'));
 
 		//create horizontal box
-		$hboxentry = new GtkHBox();
+//		$hbox0 = new GtkHBox();
 		$hbox1 = new GtkHBox();
 		$hbox2 = new GtkHBox();
 		$hbox3 = new GtkHBox();
 		$hbox4 = new GtkHBox();
+//		$hbox5 = new GtkHBox();
+//		$hbox6 = new GtkHBox();
 
+		$vbox = new GtkVBox();
 		//create vertical box
-		$vbox1 = new GtkVBox();
+		/*$vbox1 = new GtkVBox();
 		$vbox2 = new GtkVBox();
 		$vbox3 = new GtkVBox();
 		$vbox4 = new GtkVBox();
 		$vbox5 = new GtkVBox();
 		$vbox6 = new GtkVBox();
+*/
+		$entry = new GtkEntry();
 
-		$hboxentry = new GtkEntry();
 
 		//create button
 		$btn1 = new GtkButton();
 		$btn2 = new GtkButton();
 		$btn3 = new GtkButton();
+		$btnsin = new GtkButton();
+		$btncos = new GtkButton();
 		$btn4 = new GtkButton();
 		$btn5 = new GtkButton();
 		$btn6 = new GtkButton();
+		$btnt2 = new GtkButton();
+		$btnrad = new GtkButton();
 		$btn7 = new GtkButton();
 		$btn8 = new GtkButton();
 		$btn9 = new GtkButton();
@@ -41,6 +49,8 @@ class cgui {
 		$btndot = new GtkButton();
 		$btnpercent = new GtkButton();
 		$btnplus = new GtkButton();
+		$btntan = new GtkButton();
+		$btncot = new GtkButton();
 		$btnminus = new GtkButton();
 		$btndiv = new GtkButton();
 		$btnmulti = new GtkButton();
@@ -64,60 +74,82 @@ class cgui {
 		$btndot->set_label('.');
 		$btnpercent->set_label('%');
 		$btnplus->set_label('+');
-		$btnplus->set_size_request(50,60);
+		//$btnplus->set_size_request(50,60);
 		$btnminus->set_label('-');
 		$btndiv->set_label('/');
 		$btnmulti->set_label('*');
 		$btnequal->set_label('=');
-		$btnequal->set_size_request(50,60);
-		$btnref->set_size_request(50,60);
-		$btnback->set_size_request(50,60);
+		//$btnequal->set_size_request(50,60);
+		$btnt2->set_label('^2');
+		$btnsin->set_label('sin');
+		$btnsin->set_label('cos');
+		$btnsin->set_label('tan');
+		$btnsin->set_label('cot');
+		//$btnref->set_size_request(50,60);
+		//$btnback->set_size_request(50,60);
+
 
 		//add button in vbox
-		$vbox1->pack_start($btn7,false,false);
-		$vbox1->pack_start($btn4,false,false);
-		$vbox1->pack_start($btn1,false,false);
-		$vbox1->pack_start($btn0,false,false);
-		$vbox2->pack_start($btn8,false,false);
-		$vbox2->pack_start($btn5,false,false);
-		$vbox2->pack_start($btn2,false,false);
-		$vbox3->pack_start($btn9,false,false);
-		$vbox3->pack_start($btn6,false,false);
-		$vbox3->pack_start($btn3,false,false);
-		$vbox3->pack_start($btndot,false,false);
-		$vbox4->pack_start($btnpercent,false,false);
-		$vbox4->pack_start($btnmulti,false,false);
-		$vbox4->pack_start($btndiv,false,false);
-		$vbox4->pack_start($btnminus,false,false);
-		$vbox5->pack_start($btnref,false,false);
-		$vbox5->pack_start($btnplus,false,false);
-		$vbox6->pack_start($btnback,false,false);
-		$vbox6->pack_start($btnequal,false,false);
+		$hbox1->pack_start($btn7,true,true);
+		$hbox1->pack_start($btn8,true,true);
+		$hbox1->pack_start($btn9,true,true);
+		$hbox1->pack_start($btnmulti,true,true);
+		$hbox1->pack_start($btnref,true,true);
+		$hbox1->pack_start($btnback,true,true);
+		$hbox2->pack_start($btn4,true,true);
+		$hbox2->pack_start($btn5,true,true);
+		$hbox2->pack_start($btn6,true,true);
+		$hbox2->pack_start($btndiv,true,true);
+		$hbox2->pack_start($btnt2,true,true);
+		$hbox2->pack_start($btnpercent,true,true);
+		$hbox3->pack_start($btn1,true,true);
+		$hbox3->pack_start($btn2,true,true);
+		$hbox3->pack_start($btn3,true,true);
+		$hbox3->pack_start($btnminus,true,true);
+		$hbox3->pack_start($btnsin,true,true);
+		$hbox3->pack_start($btncos,true,true);
+		//$hbox3->pack_start($btn,true,true);
+		$hbox4->pack_start($btn0,true,true);
+		$hbox4->pack_start($btndot,true,true);
+		$hbox4->pack_start($btnequal,true,true);
+		$hbox4->pack_start($btnplus,true,true);
+		$hbox4->pack_start($btntan,true,true);
+		$hbox4->pack_start($btncot,true,true);
 
+		$vbox->pack_start($entry,false,false,0);
 		//add vbox in hbox
-		$hbox1->pack_start($vbox1,true);
-		$hbox2->pack_start($vbox1,true);
-		$hbox3->pack_start($vbox1,true);
-		$hbox4->pack_start($vbox1,true);
-		$hbox1->pack_start($vbox2,true);
-		$hbox2->pack_start($vbox2,true);
-		$hbox3->pack_start($vbox2,true);
-		$hbox1->pack_start($vbox3,true);
-		$hbox2->pack_start($vbox3,true);
-		$hbox3->pack_start($vbox3,true);
-		$hbox4->pack_start($vbox3,true);
-		$hbox1->pack_start($vbox4,true);
-		$hbox2->pack_start($vbox4,true);
-		$hbox3->pack_start($vbox4,true);
-		$hbox4->pack_start($vbox4,true);
-		$hbox1->pack_start($vbox5,true);
-		$hbox1->pack_start($vbox6,true);
-		$hbox2->pack_start($vbox5,true);
-		$hbox2->pack_start($vbox6,true);
+		$vbox->pack_end($hbox4,true,true);
+		$vbox->pack_end($hbox4,true,true);
+		$vbox->pack_end($hbox4,true,true);
+		$vbox->pack_end($hbox4,true,true);
+		$vbox->pack_end($hbox4,true,true);
+		$vbox->pack_end($hbox4,true,true);
+		$vbox->pack_end($hbox3,true,true);
+		$vbox->pack_end($hbox3,true,true);
+		$vbox->pack_end($hbox3,true,true);
+		$vbox->pack_end($hbox3,true,true);
+		$vbox->pack_end($hbox3,true,true);
+		$vbox->pack_end($hbox3,true,true);
+		$vbox->pack_end($hbox2,true,true);
+		$vbox->pack_end($hbox2,true,true);
+		$vbox->pack_end($hbox2,true,true);
+		$vbox->pack_end($hbox2,true,true);
+		$vbox->pack_end($hbox2,true,true);
+		$vbox->pack_end($hbox2,true,true);
+		$vbox->pack_end($hbox1,true,true);
+		$vbox->pack_end($hbox1,true,true);
+		$vbox->pack_end($hbox1,true,true);
+		$vbox->pack_end($hbox1,true,true);
+		$vbox->pack_end($hbox1,true,true);
+		$vbox->pack_end($hbox1,true,true);
+		//$vbox->pack_end($hbox5,true,true);
+		//$vbox->pack_end($hbox6,true,true);
+		//$hbox0->pack_start($entry,true);
 
 
 		//add hbox in window
-		$win->add($hbox1);
+		//$win->add($hbox0);
+		$win->add($vbox);
 		$win->show_all();
 		
 		
